3ae1eb8cc7f9e3e48ae0d99758a9989b9485ab95
Passenger-Han <le__blanc__@naver.com>
2022-09-22 18:17:30 +0900

    20220922:: Still WIP..

--- 11site_hira/js/script.js
+++ 11site_hira/js/script.js
@@ -8,9 +8,31 @@ const sct01Swiper = new Swiper('#section-01 .swiper', {
     mousewheel: false,
 });
 
+// page-3 Swipers
+const sct03Swiper01 = new Swiper ('#section-03 .banner-wrapper .swiper', {
+    slidesPerView: 1,
+    allowTouchMove: false,
+
+    pagination: {
+        el: '#section-03 .banner-wrapper .swiper-pagination',
+        clickable: true,
+    },
+})
+
+const sct03Swiper02 = new Swiper ('#section-03 .sns-wrapper .swiper', {
+    // loop: true,
+    slidesPerView: 4,
+    spaceBetween: 16,
+
+    pagination: {
+        el: '#section-03 .sns-wrapper .swiper-pagination',
+        clickable: true,
+    },
+})
+
 // Fullpage initialization
 $('#fullpage').fullpage({
     menu: 'header',
-    anchors: ['page-1', 'page-2'],
+    anchors: ['page-1', 'page-2', 'page-3', 'page-4'],
     navigation: true,
 });
