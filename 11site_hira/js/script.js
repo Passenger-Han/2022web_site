1046239846f9bf08cb5530ccdeb2263f4c5b3331
Passenger-Han <le__blanc__@naver.com>
2022-09-27 18:23:44 +0900

    20220927:: WIP..

--- 11site_hira/js/script.js
+++ 11site_hira/js/script.js
@@ -35,17 +35,60 @@ $('.background-overlay').on('mouseenter', function(){
     });
 });
 
-// $('header .background-fulldrop').on('mouseleave', function(){
-//     $('header #header-navigator .navigator .submenu').css({
-//         'display': 'none',
-//     });
-//     $('.background-fulldrop').css({
-//         'display': 'none',
-//     });
-//     $('.background-overlay').css({
-//         'display': 'none',
-//     });
-// });
+// navigator sitemap
+$('header #header-navigator button').on('click', function(){
+    $('.sitemap').css({
+        'display': 'block',
+    });
+    $('.background-sitemap').css({
+        'display': 'block',
+    });
+});
+$('.background-sitemap').on('click', function(){
+    $('.sitemap').css({
+        'display': 'none',
+    });
+    $('.background-sitemap').css({
+        'display': 'none',
+    });
+});
+
+// page-3 latest board
+$('#section-03 .news-menu a').on('click', function(){
+    $('#section-03 .news-menu a').removeClass('on');
+    $('#section-03 .news-menu a').eq($(this).index()).addClass('on');
+    loadNews($('#section-03 .news-menu a').index(this));
+});
+
+function loadNews(index){
+    $('#section-03 .news-list').removeClass('on')
+    $('#section-03 .news-list').eq(index).addClass('on')
+}
+
+$('#section-03 .sns-menu .sns-list li').on('click', function(){
+    $('#section-03 .sns-menu .sns-list li').removeClass('on');
+    $('#section-03 .sns-menu .sns-list li').eq($(this).index()).addClass('on');
+    loadSNS($('#section-03 .sns-menu .sns-list li').index(this));
+});
+
+function loadSNS(index){
+    $('#section-03 .sns-wrapper .swiper').removeClass('on')
+    $('#section-03 .sns-wrapper .swiper').eq(index).addClass('on')
+}
+
+
+
+// page-4 guideline-visual
+$('#section-04 .guideline-list p').on('click', function(){
+    $('#section-04 .guideline-list p').removeClass('on');
+    $('#section-04 .guideline-list p').eq($(this).index()).addClass('on');
+    loadGuidelineVisual($('#section-04 .guideline-list p').index(this));
+});
+
+function loadGuidelineVisual(index){
+    $('#section-04 .guideline-visual li').removeClass('on')
+    $('#section-04 .guideline-visual li').eq(index).addClass('on')
+}
 
 // page-1 Swiper
 const sct01Swiper = new Swiper('#section-01 .swiper', {
@@ -67,9 +110,9 @@ const sct03Swiper01 = new Swiper ('#section-03 .banner-wrapper .swiper', {
         type: 'fraction',
         clickable: true,
     },
-})
+});
 
-const sct03Swiper02 = new Swiper ('#section-03 .sns-wrapper .swiper', {
+const sct03Swiper0201 = new Swiper ('#section-03 .sns-wrapper .swiper01', {
     // loop: true,
     slidesPerView: 4,
     spaceBetween: 16,
@@ -78,7 +121,27 @@ const sct03Swiper02 = new Swiper ('#section-03 .sns-wrapper .swiper', {
         el: '#section-03 .sns-wrapper .swiper-pagination',
         clickable: true,
     },
-})
+});
+const sct03Swiper0202 = new Swiper ('#section-03 .sns-wrapper .swiper02', {
+    // loop: true,
+    slidesPerView: 4,
+    spaceBetween: 16,
+
+    pagination: {
+        el: '#section-03 .sns-wrapper .swiper-pagination',
+        clickable: true,
+    },
+});
+const sct03Swiper0203 = new Swiper ('#section-03 .sns-wrapper .swiper03', {
+    // loop: true,
+    slidesPerView: 4,
+    spaceBetween: 16,
+
+    pagination: {
+        el: '#section-03 .sns-wrapper .swiper-pagination',
+        clickable: true,
+    },
+});
 
 // Fullpage initialization
 $('#fullpage').fullpage({
@@ -86,7 +149,7 @@ $('#fullpage').fullpage({
     showActiveTooltip: true,
     // anchors: ['page-1', 'page-2', 'page-3', 'page-4'],
     navigation: true,
-    normalScrollElements: '#section-04 .guideline-menu .guideline-list',
+    normalScrollElements: '#section-04 .guideline-menu .guideline-list, .sitemap',
 });
 
 $('#fp-nav ul > li:last-child').css('display', 'none');
